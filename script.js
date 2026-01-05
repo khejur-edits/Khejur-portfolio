a3658ab4897c4f6feaeb42737cd8322ab15ed76b
khejur-edits <thernstech@gmail.com>
2026-01-05 11:18:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CAROUSEL SCROLL
 document.querySelectorAll('.carousel-wrapper').forEach(wrapper=>{
   const carousel = wrapper.querySelector('.carousel');
   const left = wrapper.querySelector('.left');
@@ -11,3 +12,32 @@ document.querySelectorAll('.carousel-wrapper').forEach(wrapper=>{
     carousel.scrollBy({ left: 240, behavior: 'smooth' });
   });
 });
+
+
+// YOUTUBE AUTO PAUSE LOGIC
+let players = [];
+
+function onYouTubeIframeAPIReady() {
+  document.querySelectorAll('.player').forEach((el, index) => {
+    players[index] = new YT.Player(el, {
+      videoId: el.dataset.videoId,
+      playerVars: {
+        modestbranding: 1,
+        rel: 0
+      },
+      events: {
+        onStateChange: onPlayerStateChange
+      }
+    });
+  });
+}
+
+function onPlayerStateChange(event) {
+  if (event.data === YT.PlayerState.PLAYING) {
+    players.forEach(player => {
+      if (player !== event.target) {
+        player.pauseVideo();
+      }
+    });
+  }
+}
